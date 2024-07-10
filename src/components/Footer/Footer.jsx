11ca861ba9d77bbb9fe806d8777068718a0aaecc
Yuna Hu <yunaa.765@gmail.com>
2024-07-10 16:44:02 -0700

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,20 @@ import styled from "styled-components";
 
 const Container = styled.div`
   height: 101px;
+  background-color: ${(props) => props.theme.lightGrey};
+  color: ${(props) => props.theme.darkGrey};
 `;
 
+const Content = styled.div``;
+
 const Footer = () => {
-  return <Container>Footer</Container>;
+  return (
+    <Container>
+      <Content>
+        <div>© 2024 Team Ushuaia. All rights reserved.</div>
+      </Content>
+    </Container>
+  );
 };
 
 export default Footer;
--- src/theme/theme.jsx
+++ src/theme/theme.jsx
@@ -1,5 +1,7 @@
 const theme = {
   grey: "#666666",
+  lightGrey: "#F2F2F2",
+  darkGrey: "#979797",
 };
 
 export default theme;
