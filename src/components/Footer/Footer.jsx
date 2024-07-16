e0e8f5b8ba88814c04cd66a34f684e376567f5aa
Yujie Peng <ypeng24@mylangara.ca>
2024-07-15 17:25:55 -0700

    Update app layout with new pages and components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,51 @@
+import { LuGithub } from "react-icons/lu";
 import styled from "styled-components";
 
-const Container = styled.div`
-  height: 101px;
+import Logo from "../Logo";
+
+const StyledFooter = styled.footer`
+  height: 100px;
   background-color: ${(props) => props.theme.lightGrey};
   color: ${(props) => props.theme.darkGrey};
+  padding: 2.4rem 0;
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+    height: fit-content;
+    gap: 0.8rem;
+  }
 `;
 
-const Content = styled.div``;
+const StyledLink = styled.a`
+  display: flex;
+  gap: 0.8rem;
+  align-items: center;
+  transition: color 0.3s;
+
+  &:hover {
+    color: var(--color-brand-600);
+    text-decoration: underline;
+  }
+`;
 
 const Footer = () => {
   return (
-    <Container>
-      <Content>
-        <div>© 2024 Team Ushuaia. All rights reserved.</div>
-      </Content>
-    </Container>
+    <StyledFooter>
+      <div>© 2024 Team Ushuaia. All rights reserved.</div>
+      <Logo />
+      <StyledLink
+        href="https://github.com/yunahu/purrfect-neighbors.git"
+        target="_blank"
+      >
+        <LuGithub size="2rem" />
+        <span>Find Us On GitHub</span>
+      </StyledLink>
+    </StyledFooter>
   );
 };
 
